46663064b605a2d67f1262fd4bafa7e0a078f295
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-23 17:16:36 -0300

    xD

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,17 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
-
-export default App
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='George' />
+      <Hello name='Daisy' />
+    </div>
+  )
+}
+export default App;
