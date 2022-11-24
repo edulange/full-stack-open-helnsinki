09b94d9dd323eafdc0d025455b84daeaf02dce35
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-24 17:14:33 -0300

    topp

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,24 @@
 const Header = (props) => {
+	console.log(props);
 	return <h1>{props.course}</h1>;
 };
 
 const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-const Content = (props) => {
-  const part1 = "Fundamentals of React";
-  const part2 = "Using props to pass data";
-  const part3 = "State of a component";
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
 	return (
-    <div>
+		<p>
+			{props.part} {props.exercises}
+		</p>
+	);
+};
+const Content = () => {
+	const part1 = "Fundamentals of React";
+	const part2 = "Using props to pass data";
+	const part3 = "State of a component";
+	const exercises1 = 10;
+	const exercises2 = 7;
+	const exercises3 = 14;
+	return (
+		<div>
 			<Part part={part1} exercises={exercises1} />
 			<Part part={part2} exercises={exercises2} />
 			<Part part={part3} exercises={exercises3} />
@@ -26,8 +27,8 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
-  return (
-    <p>
+	return (
+		<p>
 			Number of exercises{" "}
 			{props.exercises1 + props.exercises2 + props.exercises3}
 		</p>
@@ -35,15 +36,15 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
-  
+	const course = "Half Stack application development";
+	const exercises1 = 10;
+	const exercises2 = 7;
+	const exercises3 = 14;
+
 	return (
 		<div>
 			<Header course={course} />
-			<Content/>
+			<Content />
 			<Total
 				exercises1={exercises1}
 				exercises2={exercises2}
