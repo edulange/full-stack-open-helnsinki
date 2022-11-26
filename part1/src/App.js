65ede5d43227b8b874b205d6f730c5c666fe1375
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-25 23:35:24 -0300

    finnaly xD

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,8 @@
 const App = (props) => {
-	const {counter} = props
-	return (
-	  <div>{counter}</div>
-	)
-  }
-  
-  export default App
+  const {counter} = props
+  return (
+    <div>{counter}</div>
+  )
+}
+
+export default App
