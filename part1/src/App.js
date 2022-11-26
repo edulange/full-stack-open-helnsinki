a13286cef9c1dff916f0bba4b1d267f17ca2b8b4
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-25 23:28:48 -0300

    jesus

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,61 +1,8 @@
-const Header = props => {
-	console.log(props);
-	return <h1>{props.course}</h1>;
-};
-
-const Part = props => {
+const App = (props) => {
+	const {counter} = props
 	return (
-		<p>
-			{props.parts.name} {props.parts.exercises}
-		</p>
-	);
-};
-const Content = props => {
-
-	return (
-		<div>
-			<Part parts={props.parts[0]}/>
-			<Part parts={props.parts[1]}/>
-			<Part parts={props.parts[2]}/>
-		</div>
-	);
-};
-
-const Total = props => {
-	return (
-		<p>
-			Number of exercises{" "}
-			{props.parts.reduce((total, part) => part.exercises + total, 0)}
-		</p>
-	);
-};
-
-const App = () => {
-	const course = {
-		name: 'Half Stack application development',
-		parts: [
-		  {
-			name: 'Fundamentals of React',
-			exercises: 10
-		  },
-		  {
-			name: 'Using props to pass data',
-			exercises: 7
-		  },
-		  {
-			name: 'State of a component',
-			exercises: 14
-		  }
-		]
-	  }
-	
-	return (
-		<div>
-			<Header course={course.name} />
-			<Content parts={course.parts}/>
-			<Total parts={course.parts}/>
-		</div>
-	);
-};
-console.log(`let's study!!`)
-export default App;
+	  <div>{counter}</div>
+	)
+  }
+  
+  export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,4 +1,10 @@
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+let counter = 1
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App counter={counter} />
+)
