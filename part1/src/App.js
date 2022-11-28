321a74d6672d2b789ea4917a6769def463663ba8
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-28 10:30:08 -0300

    beautiful

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Display = ({ counter }) =><div>{counter}</div>;
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+	const [counter, setCounter] = useState(0);
 
-  return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={() => console.log('clicked')}>
-        plus
-      </button>
-    </div>
-  )
-}
-export default App
+	const increaseByOne = () => setCounter(counter + 1);
+	const decreaseByOne = () => setCounter(counter - 1);
+	const setToZero = () => setCounter(0);
+
+	return (
+		<div>
+			<Display counter={counter} />
+			<Button onClick={increaseByOne} text="plus" />
+			<Button onClick={setToZero} text="zero" />
+			<Button onClick={decreaseByOne} text="minus" />
+		</div>
+	);
+};
+export default App;
--- part1/src/index.js
+++ part1/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
