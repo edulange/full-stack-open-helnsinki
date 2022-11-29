c193ba077472232e7276f29938823fa199b137e9
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-29 20:51:00 -0300

    tu é o cara duduzeira

--- part1/ex 1.06-1.14/unicafe/src/App.js
+++ part1/ex 1.06-1.14/unicafe/src/App.js
@@ -1,51 +1,74 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Button = ({ text, handleClick }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+	<button onClick={handleClick}>{text}</button>
+);
 
-const Display = ({text, value}) => <p>{text}: {value}</p>
+const Display = ({ text, value }) => (
+	<p>
+		{text}: {value}
+	</p>
+);
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-   
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+	const [all, setAll] = useState(0);
 
-  const setToGood = (incrimental, all) => {
-    setGood(incrimental)
-    setAll(all)
-  }
-  const setToNeutral = (incrimental, all) => {
-    setNeutral(incrimental)
-    setAll(all)
-  }
-  const setToBad = (incrimental, all) => {
-    setBad(incrimental)
-    setAll(all)
-  }
+	const setToGood = (incrimental, all) => {
+		setGood(incrimental);
+		setAll(all);
+	};
+	const setToNeutral = (incrimental, all) => {
+		setNeutral(incrimental);
+		setAll(all);
+	};
+	const setToBad = (incrimental, all) => {
+		setBad(incrimental);
+		setAll(all);
+	};
 
+	const averageUnit = (good - bad) / all || 0;
+	const positiveFeedback = (good * 100) / all || 0;
 
-console.log('good', good, 'neutral', neutral,  'bad', bad)
-  return (
-    <div>
-      <h1>Give Feedback</h1>
-      <Button handleClick={() => setToGood((good + 1 ), (all + 1))} text="Good"/>
-      <Button handleClick={() => setToNeutral((neutral + 1 ), (all + 1))} text="Neutral"/>
-      <Button handleClick={() => setToBad((bad + 1 ), (all + 1))} text="Bad"/>
-      <h2>Statistics</h2>
-      <Display text="good"value={good}/>
-      <Display text="neutral"value={neutral}/>
-      <Display text="bad"value={bad}/>
-      <Display text="all"value={all}/>
-      <Display text="average"value={average}/>
-    </div>
-  )
-}
+	console.log(
+		"good",
+		good,
+		"neutral",
+		neutral,
+		"bad",
+		bad,
+		"average unit",
+		averageUnit,
+		"positive feedback",
+		positiveFeedback
+	);
+	return (
+		<div>
+			<h1>Give Feedback</h1>
+			<Button
+				handleClick={() => setToGood(good + 1, all + 1)}
+				text="Good"
+			/>
+			<Button
+				handleClick={() => setToNeutral(neutral + 1, all + 1)}
+				text="Neutral"
+			/>
+			<Button handleClick={() => setToBad(bad + 1, all + 1)} text="Bad" />
+			<h2>Statistics</h2>
+			<Display text="good" value={good} />
+			<Display text="neutral" value={neutral} />
+			<Display text="bad" value={bad} />
+			<Display text="all" value={all} />
+			<Display text="average" value={averageUnit.toFixed(2)} />
+			<Display
+				text="positive feedback"
+				value={positiveFeedback.toFixed(2) + "%"}
+			/>
+		</div>
+	);
+};
 
-export default App
+export default App;
