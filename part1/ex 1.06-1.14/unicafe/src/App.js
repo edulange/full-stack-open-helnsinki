29a9c1d49538d8744e80d2dea4c642433b655a79
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-29 17:09:41 -0300

    1.06

--- part1/ex 1.06-1.14/unicafe/src/App.js
+++ part1/ex 1.06-1.14/unicafe/src/App.js
@@ -1,14 +1,44 @@
 import { useState } from 'react'
 
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = ({text, value}) => <p>{text}: {value}</p>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+   
 
+  const setToGood = incrimental => {
+    console.log('value now', incrimental)
+    setGood(incrimental)
+  }
+  const setToNeutral = incrimental => {
+    console.log('value now', incrimental)
+    setNeutral(incrimental)
+  }
+  const setToBad = incrimental => {
+    console.log('value now', incrimental)
+    setBad(incrimental)
+  }
+    
+console.log(good, neutral, bad)
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="Good"/>
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick={() => setToBad(bad + 1)} text="Bad"/>
+      <h2>Statistics</h2>
+      <Display text="good"value={good}/>
+      <Display text="neutral"value={neutral}/>
+      <Display text="bad"value={bad}/>
     </div>
   )
 }
