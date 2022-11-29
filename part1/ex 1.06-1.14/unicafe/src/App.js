43a0ccc77a66f890a35cb31cd87af54cc8f0cafd
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-29 17:38:32 -0300

    1.07 + 1/2

--- part1/ex 1.06-1.14/unicafe/src/App.js
+++ part1/ex 1.06-1.14/unicafe/src/App.js
@@ -13,32 +13,37 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
    
 
-  const setToGood = incrimental => {
-    console.log('value now', incrimental)
+  const setToGood = (incrimental, all) => {
     setGood(incrimental)
+    setAll(all)
   }
-  const setToNeutral = incrimental => {
-    console.log('value now', incrimental)
+  const setToNeutral = (incrimental, all) => {
     setNeutral(incrimental)
+    setAll(all)
   }
-  const setToBad = incrimental => {
-    console.log('value now', incrimental)
+  const setToBad = (incrimental, all) => {
     setBad(incrimental)
+    setAll(all)
   }
-    
-console.log(good, neutral, bad)
+
+
+console.log('good', good, 'neutral', neutral,  'bad', bad)
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={() => setToGood(good + 1)} text="Good"/>
-      <Button handleClick={() => setToNeutral(neutral + 1)} text="Neutral"/>
-      <Button handleClick={() => setToBad(bad + 1)} text="Bad"/>
+      <Button handleClick={() => setToGood((good + 1 ), (all + 1))} text="Good"/>
+      <Button handleClick={() => setToNeutral((neutral + 1 ), (all + 1))} text="Neutral"/>
+      <Button handleClick={() => setToBad((bad + 1 ), (all + 1))} text="Bad"/>
       <h2>Statistics</h2>
       <Display text="good"value={good}/>
       <Display text="neutral"value={neutral}/>
       <Display text="bad"value={bad}/>
+      <Display text="all"value={all}/>
+      <Display text="average"value={average}/>
     </div>
   )
 }
