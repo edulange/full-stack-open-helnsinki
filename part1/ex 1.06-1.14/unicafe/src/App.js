037848f4295eb33c4a970712c8b18cb2e2b435b5
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-30 17:28:08 -0300

    1.11

--- part1/ex 1.06-1.14/unicafe/src/App.js
+++ part1/ex 1.06-1.14/unicafe/src/App.js
@@ -2,25 +2,34 @@ import { useState } from "react";
 
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
-const StatisticLine = ({ text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => (
+	<tr>
+		<td>{text}</td>
+		<td>{value}</td>
+	</tr>
+);
 
 const Statistics = ({ good, neutral, bad }) => {
 	const all = good + neutral + bad;
 	const average = all > 0 ? ((good - bad) / all).toFixed(2) : 0;
-	const positive = good > 0 ? ((good * 100) / all).toFixed(2) +"%" : 0;
+	const positive = good > 0 ? ((good * 100) / all).toFixed(2) + "%" : 0;
 
-	return (
-		all > 0 ? (
+	return all > 0 ? (
 		<>
 			<h2>Statistics</h2>
-			<StatisticLine text="good" value={good}/>
-			<StatisticLine text="neutral" value={neutral}/>
-			<StatisticLine text="bad" value={bad}/>
-			<StatisticLine text="all" value={all}/>
-			<StatisticLine text="average" value={average}/>
-			<StatisticLine text="positive" value={positive}/>
+			<table>
+				<tbody>
+					<StatisticLine text="good" value={good} />
+					<StatisticLine text="neutral" value={neutral} />
+					<StatisticLine text="bad" value={bad} />
+					<StatisticLine text="all" value={all} />
+					<StatisticLine text="average" value={average} />
+					<StatisticLine text="positive" value={positive} />
+				</tbody>
+			</table>
 		</>
-		) : <p>No feedbacks given yet</p>
+	) : (
+		<p>No feedbacks given yet</p>
 	);
 };
 
