87a6bce13a74430eaf56444ffa3bfc95c8ba8eb5
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-12-11 18:25:09 -0300

    ex 2.1

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,38 +1,51 @@
-import Course from './components/Course'
+const Header = ({ title }) => <h1>{title}</h1>;
+const Part = ({ part }) => (
+	<p>
+		{part.name} {part.exercises}
+	</p>
+);
+const Content = ({ parts }) => (
+	<div>
+		{parts.map((part) => (
+			<Part part={part} key={part.id} />
+		))}
+	</div>
+);
 
+const Course = ({ course }) => {
+	return (
+		<>
+			<Header title={course.name} />
+			<Content parts={course.parts} />
+		</>
+	);
+};
 
+//app -> Course-> header + content => part + part
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-const { parts } = course // assim evita de eu precisar escrever "course.parts.map"
+	const course = {
+		id: 1,
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+				id: 1,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+				id: 2,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+				id: 3,
+			},
+		],
+	};
 
-  return (
-    <div>
-      <h1>{course.name}</h1>
-        {parts.map(part => 
-          <Course key={part.id} name={part.name} exercises={part.exercises} />
-        )}
-    </div>
-  )
-}
+	return <Course course={course} />;
+};
 
-export default App
+export default App;
