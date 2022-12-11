a6c687ca26f52ea772a2ca3d5163270a33db64d7
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-12-11 19:04:01 -0300

    finishing 2.5 yaaaaaay

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,58 +1,58 @@
-const Header = ({ title }) => <h1>{title}</h1>;
-const Part = ({ part }) => (
-	<p>
-		{part.name} {part.exercises}
-	</p>
-);
+import Course from './components/Course'
 
-const Content = ({ parts }) => (
-	<div>
-		{parts.map((part) => (
-			<Part part={part} key={part.id} />
-		))}
-    <p>
-      Total of {parts.reduce((acc, {exercises}) => acc + exercises, 0)} exercises
-    </p>
-	</div>
-);
-//jesus christ xDDD, this is awesome
-
-const Course = ({ course }) => {
-	return (
-		<>
-			<Header title={course.name} />
-			<Content parts={course.parts} />
-		</>
-	);
-};
-
-//app -> Course-> header + content => part + part
 const App = () => {
-	const course = {
-		id: 1,
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-				id: 1,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-				id: 2,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-				id: 3,
-			},
-		],
-	};
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-console.log('course.parts', course.parts.reduce((acc, {exercises}) => acc + exercises, 0))
-
-	return <Course course={course} />;
+	return (
+    <div>
+      <h1>Web development curriculum</h1>
+      <Course course={courses[0]} />
+      <Course course={courses[1]} />
+    </div> 
+  )
 };
 
 export default App;
