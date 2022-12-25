59395111f593a879187de5f6b5948d6d45418ef4
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-12-25 15:59:39 -0300

    2.8 numbers

--- part2/exercises/phonebook/src/App.js
+++ part2/exercises/phonebook/src/App.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
 const Numbers = ({ persons }) => {
-	return <li>{persons.name}</li>;
+	return (
+		<li>
+			{persons.name} {persons.number}
+		</li>
+	);
 };
 
 const App = (props) => {
 	const [persons, setPersons] = useState(props.persons);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const addName = (event) => {
 		//o addname só vai ser executado ao ser submitado
@@ -16,19 +21,24 @@ const App = (props) => {
 			const nameObject = {
 				name: newName,
 				id: persons.length + 1,
+				number: newNumber,
 			};
 			setPersons(persons.concat(nameObject));
 			setNewName("");
+			setNewNumber("");
 		} else {
-      event.preventDefault();
-      alert(`${newName} is already added to phonebook`)
+			event.preventDefault();
+			alert(`${newName} is already added to phonebook`);
 			setNewName("");
-    }
+		}
 	};
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value);
-		//sinceramente to meio boiando com essa parada
+		//agora eu entendi, eu preicso fazer isso para ele renderizar a colocação de CADA letra
+	};
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
 	};
 
 	return (
@@ -38,6 +48,11 @@ const App = (props) => {
 				<div>
 					name: <input value={newName} onChange={handleNameChange} />
 				</div>
+				<div>
+					number:
+					<input value={newNumber} onChange={handleNumberChange} />
+				</div>
+
 				<div>
 					<button type="submit">add</button>
 				</div>
--- part2/exercises/phonebook/src/index.js
+++ part2/exercises/phonebook/src/index.js
@@ -2,8 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const persons = [
-	{ name: "Arto Hellas", id: 1 },
-	{ name: "Testando", id: 2 },
+	{ name: "Arto Hellas", id: 1, number: 998309240 },
+	{ name: "Dudu", id: 2, number: 997308699 },
 ];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
