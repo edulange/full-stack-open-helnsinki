06d0363b3ef20db4391dc3674249c69fc8e38939
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-12-25 15:45:36 -0300

    2.7 xDDD lets do this!

--- part2/exercises/phonebook/src/App.js
+++ part2/exercises/phonebook/src/App.js
@@ -1,49 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Numbers = ( {persons} ) => {
-  return (
-    <li>{persons.name}</li>
-  )
-}
+const Numbers = ({ persons }) => {
+	return <li>{persons.name}</li>;
+};
 
 const App = (props) => {
-  const [persons, setPersons] = useState(props.persons) 
-  const [newName, setNewName] = useState('')
+	const [persons, setPersons] = useState(props.persons);
+	const [newName, setNewName] = useState("");
 
-  const addName = (event) => {
-    event.preventDefault()
-    const nameObject = {
-      name: newName,
-      id: persons.length + 1
+	const addName = (event) => {
+		//o addname só vai ser executado ao ser submitado
+		const checkName = persons.some((person) => newName === person.name);
+		if (!checkName) {
+			event.preventDefault();
+			const nameObject = {
+				name: newName,
+				id: persons.length + 1,
+			};
+			setPersons(persons.concat(nameObject));
+			setNewName("");
+		} else {
+      event.preventDefault();
+      alert(`${newName} is already added to phonebook`)
+			setNewName("");
     }
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-  }
+	};
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-  
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-         <Numbers persons={person} key={person.id} /> 
-          )}
-      </ul>
-    </div>
-  )
-}
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+		//sinceramente to meio boiando com essa parada
+	};
 
-export default App
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addName}>
+				<div>
+					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			<ul>
+				{persons.map((person) => (
+					<Numbers persons={person} key={person.id} />
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default App;
