8e8dd7d786b4026b549c065c0e796f93a5aa191d
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-01-16 20:15:07 -0300

    refactoring

--- part2/exercises/countries/src/App.js
+++ part2/exercises/countries/src/App.js
@@ -3,37 +3,45 @@ import axios from "axios";
 import Filter from "./Components/Filter";
 import CountryList from "./Components/CountryList";
 
-
 const App = () => {
-    
-    const [countries, setCountries] = useState([]);//os dados sao armazenados aqui
-    const [newFilter, setNewFilter] = useState("");
+	const [countries, setCountries] = useState([]); //os dados sao armazenados aqui
+	const [newFilter, setNewFilter] = useState("");
 
-    //Handle do input do usuário
-    const handleChange = (setValue) => (event) => setValue(event.target.value);
+	//Handle do input do usuário
+	const handleChange = (setValue) => (event) => setValue(event.target.value);
 
-    //API é chamada e dados são armazenados no estado
-    useEffect(() => {
-        axios.get("https://restcountries.com/v3.1/all").then((response) => {
-            setCountries(response.data);
-        });
-    }, []);
+	//API é chamada e dados são armazenados no estado
+	useEffect(() => {
+		axios.get("https://restcountries.com/v3.1/all").then((response) => {
+			setCountries(response.data);
+		});
+	}, []);
 
-    //Filtro para países com base na entrada do usuário
-    const filteredCountries = countries.filter(country => {
-        return country.name.common.toLowerCase().includes(newFilter.toLowerCase());
-    });
-    
-    let countryToDisplay = null;
-if (filteredCountries.length === 1) {
-    countryToDisplay = filteredCountries[0];
-}
+	//Filtro para países com base na entrada do usuário
+	const filteredCountries = countries.filter((country) => {
+		return country.name.common
+			.toLowerCase()
+			.includes(newFilter.toLowerCase());
+	});
 
-    return (
-        <div>
-            <Filter value={newFilter} handleChange={handleChange(setNewFilter)} />
-            <CountryList filteredCountries={filteredCountries} countries={countries} newFilter={newFilter} countryToDisplay={countryToDisplay}/>
-        </div>
-    );
+	let countryToDisplay = null;
+	if (filteredCountries.length === 1) {
+		countryToDisplay = filteredCountries[0];
+	}
+
+	return (
+		<div>
+			<Filter
+				value={newFilter}
+				handleChange={handleChange(setNewFilter)}
+			/>
+			<CountryList
+				filteredCountries={filteredCountries}
+				countries={countries}
+				newFilter={newFilter}
+				countryToDisplay={countryToDisplay}
+			/>
+		</div>
+	);
 };
 export default App;
