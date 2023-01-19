98a57f7cb53dcb90fcb19e4cd6a6c28769918f99
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-01-18 22:08:02 -0300

    finishing xD

--- part2/exercises/countries/src/Components/CountryList.js
+++ part2/exercises/countries/src/Components/CountryList.js
@@ -1,12 +1,22 @@
-const CountryList = ({ filteredCountries, countryToDisplay, handleCountryClick }) => {
-    
+const CountryList = ({
+	filteredCountries,
+	countryToDisplay,
+	handleCountryClick,
+}) => {
 	if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
 		return (
 			<ul>
 				{filteredCountries.map((country) => (
-					<li key={country.name.common}>{country.name.common}
-                    <button onClick={()=>handleCountryClick(country.name.common)}>show</button>                
-                    </li>
+					<li key={country.name.common}>
+						{country.name.common}
+						<button
+							onClick={() =>
+								handleCountryClick(country.name.common)
+							}
+						>
+							show
+						</button>
+					</li>
 				))}
 			</ul>
 		);
@@ -18,16 +28,20 @@ const CountryList = ({ filteredCountries, countryToDisplay, handleCountryClick }
 				<h1>{countryToDisplay.name.common}</h1>
 				<p>Capital {countryToDisplay.capital}</p>
 				<p>Area {countryToDisplay.area} m²</p>
-                <h3>Languages:</h3>
-                <ul>
-                    {languangesArr.map((language) => (
-                        <li key={countryToDisplay}>{language}</li>
-                    ))}
-                </ul>
-                <img src={countryToDisplay.flags.png} alt="country flag" />
+				<h3>Languages:</h3>
+				<ul>
+					{languangesArr.map((language) => (
+						<li key={countryToDisplay}>{language}</li>
+					))}
+				</ul>
+				<img
+					src={countryToDisplay.flags.png}
+					alt={`Flag of ${countryToDisplay}`}
+				/>
 			</div>
 		);
 	}
+	return null
 };
 
 export default CountryList;
