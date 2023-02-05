074922f5d70463715a5c400922aecb16de9601b4
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-02-05 20:37:01 -0300

    it worked

--- part3/exercises/phonebook/index.js
+++ part3/exercises/phonebook/index.js
@@ -1,30 +1,43 @@
-console.log('tentado colocar o db')
-
 const express = require('express')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+
 const app = express()
+app.use(bodyParser.json())
+
+logger.token('body', (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(logger(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 app.use(express.json())
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
+
+console.log('até aqui ok')
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
     }
 ]
 
@@ -36,26 +49,23 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
-console.log('está funcionando ainda?')
-console.log('sim, estou vendo no cmder')
-
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-
-      if (person) {
+    
+    if (person) {
         response.json(person)
-      } else {
+    } else {
         response.status(404).end()
-      }
-    })
+    }
+})
 
 
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-
+    
     response.status(204).end()
 })
 
@@ -65,7 +75,7 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(body)
+    
 
     if(!body.name) {
         return response.status(400).json({
@@ -77,33 +87,32 @@ app.post('/api/persons', (request, response) => {
             error: 'number content missing'
         })
     }
-
-const isUniqueName = persons.find(person => body.name === person.name)
-const isUniqueNumber = persons.find(person => body.number === person.number)
-
+    
+    const isUniqueName = persons.find(person => body.name === person.name)
+    const isUniqueNumber = persons.find(person => body.number === person.number)
+    
     if(!isUniqueName) {
-        console.log("is unique")
+        console.log("name is unique")
     } else {
         return response.status(409).json({
             error: "name should be unique"
         })
     }
     if(!isUniqueNumber) {
-        console.log("is unique")
+        console.log("number is unique")
     } else {
         return response.status(409).json({
             error: "number should be unique"
         })
     }
-
+    
     const person = {
         id: generateId(),
         name: body.name,
         number: body.number
     }
-
+    
     persons = persons.concat(person)
-    console.log('person :>> ', person);
     response.json(person)
 })
 
@@ -111,8 +120,9 @@ app.get('/info', (request, response) => {
     const actualDate = new Date()
     response.send(`Phonebook has info for ${persons.length} people
     <br>${actualDate}<br>`)
-
+    
 })
+
 
 const PORT = 3001
 app.listen(PORT)
