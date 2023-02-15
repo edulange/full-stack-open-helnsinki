effb5b3fd424de869122887c53c1704d98453add
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-02-15 20:34:44 -0300

    hmmmm

--- part3-newstart/notes/src/services/notes.js
+++ part3-newstart/notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/notes'
+const baseUrl = 'https://backend-separeted.onrender.com/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
