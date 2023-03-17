6849e00f22df04358828417a6fa212e30b3fb251
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-03-16 21:34:02 -0300

    lets do it bro

---
+++ part3-newstart/phonebook/src/services/phonenumbers.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+const baseUrl = 'https://phonebook-backend-part3.onrender.com/api/persons'
+//its on babbyyyy
+
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    console.log('getAll :>>');
+    return request.then(response => response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    console.log('create');
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    console.log('update');
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+const remove = (id, newObject) => {
+    const request = axios.delete(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+
+export default { getAll, create, update, remove}
