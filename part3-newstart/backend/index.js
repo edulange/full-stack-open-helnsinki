5cb5592bccc99a897499666ec7a035a8a73dba44
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-03-19 21:47:10 -0300

    xd

--- part3-newstart/backend/index.js
+++ part3-newstart/backend/index.js
@@ -97,6 +97,3 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
-
-
-console.log('im sleepy')
--- part3-newstart/phonebook/index.js
+++ part3-newstart/phonebook/index.js
@@ -15,28 +15,7 @@ app.use(morgan((tokens, request, response) => [
     tokens['response-time'](request, response), 'ms',
     tokens.body(request, response)].join(' ')))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = []
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
