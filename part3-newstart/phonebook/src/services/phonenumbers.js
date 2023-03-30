7c9d8245081a11e75e005feffcedac23768ef060
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-03-30 20:08:33 -0300

    gogogo

--- part3-newstart/phonebook/src/services/phonenumbers.js
+++ part3-newstart/phonebook/src/services/phonenumbers.js
@@ -4,24 +4,25 @@ const baseUrl = 'https://phonebook-backend-part3.onrender.com/api/persons'
 
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
     console.log('getAll :>>');
+    const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
 const create = newObject => {
+    console.log('create, from axios');
     const request = axios.post(baseUrl, newObject)
-    console.log('create');
     return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    console.log('update');
+    console.log('update, from axios');
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
 
 const remove = (id, newObject) => {
+    console.log('remove, from axios')
     const request = axios.delete(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
