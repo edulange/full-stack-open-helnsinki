494ccc08929c6668331f27e1778a903f2bdc762d
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-04-21 21:47:29 -0300

    niceone brow

--- part4/notes/utils/config.js
+++ part4/notes/utils/config.js
@@ -1,12 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
-
-const config = require('./utils/config')
-
-// eslint-disable-next-line no-undef
-logger.info(`Server running on port ${config.PORT}`)
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
