8d04683178ed67fff4d70192f774074fa234eec6
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-04-22 17:51:39 -0300

    4.10 DONE

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -8,22 +8,47 @@ blogsRouter.get('/', async (request, response) => {
   })
 
 
-blogsRouter.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+  blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
   })
   
-  blogsRouter.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
   
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+  blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+  
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0 // Define likes como 0 se não estiver presente no objeto
+    })
+  
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  })
+
+  blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  })
+
+  blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+  
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0
+    }
+  
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
   })
 
   module.exports = blogsRouter
