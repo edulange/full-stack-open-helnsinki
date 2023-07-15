6bd60c647013f8c7470336233d9c4045cb4d5972
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-07-15 00:11:13 -0300

    updating

--- part2/exercises/countries/src/Components/CountryList.js
+++ part2/exercises/countries/src/Components/CountryList.js
@@ -11,50 +11,52 @@ axios.get(weatherAPI).then((response) => {
 https://api.openweathermap.org/data/2.5/weather?q=berlim&units=metric&lang=pt_br9230098fd00da4fd9c835976ddf4dbcc
 
 */
-const apiKey = '9230098fd00da4fd9c835976ddf4dbcc'
-
-
+const apiKey = "9230098fd00da4fd9c835976ddf4dbcc";
 
 const CountryList = ({
 	filteredCountries,
 	countryToDisplay,
 	handleCountryClick,
 }) => {
-	const [temperature, setTemperature] = useState(0)
-	const [weatherIcon, setWeatherIcon] = useState(0)
-	const [wind, setWind] = useState(0)
-	
+	const [temperature, setTemperature] = useState(0);
+	const [weatherIcon, setWeatherIcon] = useState(0);
+	const [wind, setWind] = useState(0);
 
 	if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
 		return (
-			<ul>
-				{filteredCountries.map((country) => (
-					<li key={country.name.common}>
-						{country.name.common}
-						<button
-							onClick={() =>
-								handleCountryClick(country.name.common)
-							}
-						>
-							show
-						</button>
-					</li>
-				))}
-			</ul>
+			<div className="filtered-countries-container">
+				<ul>
+					{filteredCountries.map((country) => (
+						<li key={country.name.common}>
+							{country.name.common}
+							<button
+								onClick={() =>
+									handleCountryClick(country.name.common)
+								}
+							>
+								show
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
 		);
 	}
 	if (filteredCountries.length === 1) {
-		axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${countryToDisplay.capital}&units=metric&APPID=${apiKey}`)
-		.then(response => {
-			setTemperature(Math.round(response.data.main.temp))
-			setWind(response.data.wind.speed)
-			setWeatherIcon(response.data.weather[0].icon)
-		})
+		axios
+			.get(
+				`https://api.openweathermap.org/data/2.5/weather?q=${countryToDisplay.capital}&units=metric&APPID=${apiKey}`
+			)
+			.then((response) => {
+				setTemperature(Math.round(response.data.main.temp));
+				setWind(response.data.wind.speed);
+				setWeatherIcon(response.data.weather[0].icon);
+			});
 		let languangesArr = Object.values(countryToDisplay.languages); //tratando o objeto
 		return (
-			<div>
+			<div className="country-list-container">
 				<h1>{countryToDisplay.name.common}</h1>
-				<p>Capital {countryToDisplay.capital}</p>
+				<h2>Capital {countryToDisplay.capital}</h2>
 				<p>Area {countryToDisplay.area} m²</p>
 				<h3>Languages:</h3>
 				<ul>
@@ -66,14 +68,17 @@ const CountryList = ({
 					src={countryToDisplay.flags.png}
 					alt={`Flag of ${countryToDisplay}`}
 				/>
-			<h2>Weather in {countryToDisplay.capital}</h2>
+				<h3>Weather in {countryToDisplay.capital}</h3>
 				<p>temperature {temperature} ºC</p>
-				<img src={`http://openweathermap.org/img/wn/${weatherIcon}.png`} alt="weather img" />
+				<img
+					src={`http://openweathermap.org/img/wn/${weatherIcon}.png`}
+					alt="weather img"
+				/>
 				<p>wind {wind} m/s</p>
 			</div>
 		);
 	}
-	return null
+	return null;
 };
 
 export default CountryList;
--- part5/notes/src/index.js
+++ part5/notes/src/index.js
@@ -3,7 +3,3 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
-
-console.log('gettingback on track')
-console.log('testeeeeee')
-console.log('hehehee')
