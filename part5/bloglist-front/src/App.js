ebd71e9ff397015e2b68a660b4e3aa2ac20be65a
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-07-26 21:57:46 -0300

    Update App.js

--- part5/bloglist-front/src/App.js
+++ part5/bloglist-front/src/App.js
@@ -80,7 +80,7 @@ const App = () => {
       {!user && loginForm()}
       {user && (
         <div>
-          <p>{user.name} logged in</p>
+          <p>{user.name} logged in</p> LOGOUT BUTTON
         </div>
       )}
 
