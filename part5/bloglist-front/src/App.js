d9376a7164bfcfeffc764ccf3114e76f1ab0136c
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-07-29 18:08:55 -0300

    5.4

--- part5/bloglist-front/src/App.js
+++ part5/bloglist-front/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import Notification from "./components/Notification";
 
 const App = () => {
 	const [blogs, setBlogs] = useState([]);
@@ -10,6 +11,10 @@ const App = () => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [user, setUser] = useState(null);
+
+	const [newBlogTitle, setNewBlogTitle] = useState("");
+	const [newBlogAuthor, setNewBlogAuthor] = useState("");
+	const [newBlogUrl, setNewBlogUrl] = useState("");
 
 	useEffect(() => {
 		blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -43,17 +48,53 @@ const App = () => {
 			setUsername("");
 			setPassword("");
 		} catch (exception) {
-			setErrorMessage("Wrong Credentials");
+			setErrorMessage("wrong username or password");
 			setTimeout(() => {
 				setErrorMessage(null);
 			}, 5000);
 		}
 	};
 
-  const handleLogout = (event) => {
-    window.localStorage.clear()
-    setUser(null)
-  }
+	const handleCreateBlog = async (event) => {
+		event.preventDefault();
+
+		const newBlog = {
+			title: newBlogTitle,
+			author: newBlogAuthor,
+			url: newBlogUrl,
+		};
+
+		try {
+			// faz uma chamada API para o servidorbackend para adcionar um novo blog
+			const response = await blogService.create(newBlog);
+			// assumindo que o API retorne o objeto blog com idfield
+
+			//atualizando o localstate para incluir o novo blog
+			setBlogs([...blogs, response]);
+
+			//limpando os campos após ser adicionado com sucesso
+			setNewBlogTitle("");
+			setNewBlogAuthor("");
+			setNewBlogUrl("");
+			
+			if (response) { // se a response é true
+				setErrorMessage(`a new blog ${newBlog.title} by ${newBlog.author} created`)
+				setTimeout( () => {
+					setErrorMessage(null);
+				}, 5000)
+			}
+		} catch (exception) {
+			setErrorMessage("Falha na criação do blog");
+			setTimeout(() => {
+				setErrorMessage(null);
+			}, 5000);
+		}
+	};
+
+	const handleLogout = (event) => {
+		window.localStorage.clear();
+		setUser(null);
+	};
 
 	const loginForm = () => (
 		<form onSubmit={handleLogin}>
@@ -79,28 +120,68 @@ const App = () => {
 		</form>
 	);
 
-  const newBlogs = () => (
-    <div>
-      <h2>Create new</h2>
-      Title: <input type="text" name="title" />
-    </div>
-  )
+	const newBlogs = () => (
+		<>
+			<h2>Create a new blog</h2>
+			<form onSubmit={handleCreateBlog}>
+				<div>
+					<div>
+						Title:{" "}
+						<input
+							type="text"
+							name="Title"
+							value={newBlogTitle}
+							onChange={({ target }) =>
+								setNewBlogTitle(target.value)
+							}
+						/>
+					</div>
+					<div>
+						Author:{" "}
+						<input
+							type="text"
+							name="Author"
+							value={newBlogAuthor}
+							onChange={({ target }) =>
+								setNewBlogAuthor(target.value)
+							}
+						/>
+					</div>
+					<div>
+						Url:{" "}
+						<input
+							type="text"
+							name="Url"
+							value={newBlogUrl}
+							onChange={({ target }) =>
+								setNewBlogUrl(target.value)
+							}
+						/>
+					</div>
+					<button type="submit">Create</button>
+				</div>
+			</form>
+		</>
+	);
 
 	const logOutButton = () => (
 		<div>
-			<button type="submit" onClick={handleLogout}>Logout</button>
+			<button type="submit" onClick={handleLogout}>
+				Logout
+			</button>
 		</div>
 	);
 
 	return (
 		<div>
 			<h2>Blogs</h2>
+			<Notification message={errorMessage} />
 			{!user && loginForm()}
 			{user && (
 				<div>
 					<p>{user.name} logged in</p>
 					{logOutButton()}
-          {newBlogs()}
+					{newBlogs()}
 				</div>
 			)}
 
