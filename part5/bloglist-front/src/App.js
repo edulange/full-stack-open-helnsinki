c772e0dc92ae3696d8e7b7e9f83105d80f0aceb6
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-07-29 13:30:46 -0300

    5.2

--- part5/bloglist-front/src/App.js
+++ part5/bloglist-front/src/App.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
-import loginService from './services/login'
+import loginService from "./services/login";
 
 const App = () => {
 	const [blogs, setBlogs] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null)
-
+	const [errorMessage, setErrorMessage] = useState(null);
 
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
@@ -16,73 +15,87 @@ const App = () => {
 		blogService.getAll().then((blogs) => setBlogs(blogs));
 	}, []);
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
-    }
-  }, [])
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON);
+			setUser(user);
+			blogService.setToken(user.token);
+		}
+	}, []);
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
+	const handleLogin = async (event) => {
+		event.preventDefault();
 
-    try {
-      const user = await loginService.login({
-        username,
-        password
-      })
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			});
 
-      window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      ) 
-      
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setErrorMessage('Wrong Credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
+			window.localStorage.setItem(
+				"loggedNoteappUser",
+				JSON.stringify(user)
+			);
+
+			blogService.setToken(user.token);
+			setUser(user);
+			setUsername("");
+			setPassword("");
+		} catch (exception) {
+			setErrorMessage("Wrong Credentials");
+			setTimeout(() => {
+				setErrorMessage(null);
+			}, 5000);
+		}
+	};
+
+  const handleLogout = (event) => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
 	const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
+		<form onSubmit={handleLogin}>
+			<div>
+				username
+				<input
+					type="text"
+					value={username}
+					name="Username"
+					onChange={({ target }) => setUsername(target.value)}
+				/>
+			</div>
+			<div>
+				password
+				<input
+					type="password"
+					value={password}
+					name="Password"
+					onChange={({ target }) => setPassword(target.value)}
+				/>
+			</div>
+			<button type="submit">login</button>
+		</form>
+	);
+
+	const logOutButton = () => (
+		<div>
+			<button type="submit" onClick={handleLogout}>Logout</button>
+		</div>
+	);
 
 	return (
 		<div>
 			<h2>Blogs</h2>
-      {!user && loginForm()}
-      {user && (
-        <div>
-          <p>{user.name} logged in</p> LOGOUT BUTTON
-        </div>
-      )}
+			{!user && loginForm()}
+			{user && (
+				<div>
+					<p>{user.name} logged in</p>
+					{logOutButton()}
+					{/* USAR AQUI O BOTÃO LOGOUT */}
+				</div>
+			)}
 
 			{blogs.map((blog) => (
 				<Blog key={blog.id} blog={blog} />
