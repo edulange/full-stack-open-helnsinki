0e300d941ce4b2019c888e5ee2f7adf98924a913
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-08-20 17:42:50 -0300

    até aqui tudo ok

--- part5/bloglist-front/src/App.js
+++ part5/bloglist-front/src/App.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
-import Notification from './components/Notification'
+
+import Blog from './components/Blog'
+import { SuccessNotification, ErrorNotification } from './components/Notification'
 import Togglable from './components/Togglable'
 import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [message, setErrorMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -30,27 +32,40 @@ const App = () => {
     }
   }, [])
 
-  //seta mensagem de erro no status
-  //depois de 2 segs limpa
 
-  const showMessage = (message) => {
+  //************************************************* Setting messages */
+  const showSuccessMessage = (message) => {
+    setSuccessMessage(message)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 3000)
+  }
+
+  const showErrorMessage = (message) => {
     setErrorMessage(message)
     setTimeout(() => {
       setErrorMessage(null)
     }, 3000)
   }
 
-  const updateLikes = (id, newLikes) => {
-    blogService.update(id, { likes: newLikes }).then((updatedBlog) => {
-      setBlogs((prevBlogs) =>
-        prevBlogs.map((blog) =>
-          blog.id === updatedBlog.id ? updatedBlog : blog
-        )
-      )
-    })
+  //**********************************************************************************
+  // LOGIN VIEW, LOGGING IN and LOGGING OUT
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    try {
+      showSuccessMessage(`Goodbye ${user.name}`)
+      window.localStorage.clear()
+      blogService.setToken(null)
+      setUser(null)
+    } catch (exception) {
+      showErrorMessage('something went wrong, try to logout again')
+    }
+
   }
 
-  const handleLogin = (event) => {
+  const loginUser = (event) => {
     event.preventDefault()
 
     loginService
@@ -63,17 +78,28 @@ const App = () => {
           'loggedNoteappUser',
           JSON.stringify(user)
         )
-
+        showSuccessMessage(`Welcome ${user.name}`)
         blogService.setToken(user.token)
         setUser(user)
         setUsername('')
         setPassword('')
       })
       .catch((exception) => {
-        showMessage('wrong username or password')
+        showErrorMessage('wrong username or password')
         console.log(exception)
       })
   }
+
+  const updateLikes = (id, newLikes) => {
+    blogService.update(id, { likes: newLikes }).then((updatedBlog) => {
+      setBlogs((prevBlogs) =>
+        prevBlogs.map((blog) =>
+          blog.id === updatedBlog.id ? updatedBlog : blog
+        )
+      )
+    })
+  }
+
 
   const addBlog = (blogObject) => {
     blogService
@@ -86,13 +112,13 @@ const App = () => {
 
         if (response) {
           // se a response é true
-          showMessage(
+          showErrorMessage(
             `a new blog ${blogObject.title} by ${blogObject.author} created`
           )
         }
       })
       .catch((exception) => {
-        showMessage('Falha na criação do blog')
+        showErrorMessage('Falha na criação do blog')
         console.log('exception :>> ', exception)
       })
   }
@@ -115,13 +141,9 @@ const App = () => {
         })
     }
   }
-  const handleLogout = () => {
-    window.localStorage.clear()
-    setUser(null)
-  }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
+  const handleLogin = () => (
+    <form onSubmit={loginUser}>
       <div>
 				username
         <input
@@ -155,8 +177,9 @@ const App = () => {
   return (
     <div>
       <h2>Blogs</h2>
-      <Notification message={message} />
-      {!user && loginForm()}
+      <ErrorNotification message={errorMessage} />
+      <SuccessNotification message={successMessage} />
+      {!user && handleLogin()}
       {user && (
         <div>
           <p>{user.name} logged in</p>
