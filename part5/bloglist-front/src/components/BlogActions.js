94dbc14ca81faf75f97eeee6b50ab9b776eedb06
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-08-22 06:55:12 -0300

    jesus christ lets do it bro

---
+++ part5/bloglist-front/src/components/BlogActions.js
@@ -0,0 +1,13 @@
+import blogService from '../services/blogs'
+
+// Functions that are used blog actions.
+// funções que são usadas nos blog actions
+// Blogs tem duas funções, adicionar ou remover um blog
+
+ 
+// uses blogService to add a like to blog
+// and shows a success messagge (or error message)
+
+export const addLike = (blog, setLikes, showSuccessMessage, showErrorMessage) => {
+    
+}
