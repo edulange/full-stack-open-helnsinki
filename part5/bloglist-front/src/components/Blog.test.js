a7efb13ca6bd2cbb7fb200df8da134faf86d8297
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-09-17 16:13:56 -0300

    lets do it bro

--- part5/bloglist-front/src/components/Blog.test.js
+++ part5/bloglist-front/src/components/Blog.test.js
@@ -1,53 +1,28 @@
 import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render, screen  } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-const blog = {
-  title: 'Crimes and Punishment',
-  author: 'Dudu',
-  url: 'www.test.com.br',
-  likes: 15
-}
+test('Testando se renderiza o blog', () => {
+  const blog = {
+    title: 'Teste',
+    author: 'Dudu',
+    url: 'www.teste.com.br',
+    likes: 5
+  }
 
-const blogUser = {
-  id: '69'
-}
+  const { container } = render(<Blog blog={blog} />)
+  const element = container.querySelector('.blog')
+  screen.debug()
+  
+  //const element = screen.getByText('Teste', { exact: false})
+  expect(element).toHaveTextContent('Teste')
+  expect(element).toHaveTextContent('Dudu')
+  expect(element).not.toHaveTextContent('www.teste.com.br')
+  expect(element).not.toHaveValue(5)
+})
 
-  test('renders title and author but not URL or likes by default', () => {
-  
-    const { container } = render(<Blog blog={blog} />)
-    const element = container.querySelector('.blog')
-    expect(element).toHaveTextContent('Crimes and Punishment')
-    expect(element).toHaveTextContent('Dudu')
-    expect(element).not.toHaveValue(15)
-  })
-
-  test('renders likes and url after button click', async () => {
-    const mockHandler = jest.fn()
-    const { container } = render(<Blog blog={blog} toggleDetail={mockHandler} user={blogUser}/>)
-  
-    const user = userEvent.setup()
-    const button = screen.getByText('view')
-    await user.click(button)
-    const element = container.querySelector('.blogDetail')
-    const like = screen.getByText('Likes 15', { exact: false })
-  
-    expect(element).toHaveTextContent('www.test.com.br')
-    expect(like).toBeDefined()
-  })
-
-  test('calls twice', async () => {
-    const mockHandler = jest.fn()
-    render(<Blog blog={blog} user={blogUser}>
-      <button onClick={() => mockHandler(blog.id)}>
-        like
-      </button>
-    </Blog>)
-  
-    const user = userEvent.setup()
-    const button = screen.getByRole('button')
-    await user.dblClick(button)
-    expect(mockHandler.mock.calls).toHaveLength(2)
-  })
+test('Testadno se o o URL e o número de likes é mostrado quando clicado em View', () => {
+  const user = userEvent.setup()
+})
