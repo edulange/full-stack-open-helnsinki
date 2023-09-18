f62aed52e0d2ed8600f2cab750a95147232f47fa
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-09-18 20:44:59 -0300

    cypress LETS GO

---
+++ part5/notestest/cypress/e2e/note_app.cy.js
@@ -0,0 +1,23 @@
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('log in').click()
+  })
+
+  it('user can login', function () {
+    cy.contains('log in').click()
+    cy.get('#username').type('edugod')
+    cy.get('#password').type('292044')
+    cy.get('#login-button').click()
+
+    cy.contains('Eduardo logged in')
+  })
+})
