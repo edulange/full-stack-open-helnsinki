d61d6369a2093474ec67b4fddda04a60d2964b20
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-10-07 12:21:04 -0300

    Update App.js

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,9 +2,6 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote, createAnecdote } from './actions/anecdoteActions'
 
 
-console.log('teste')
-console.log('teste')
-console.log('teste')
 const App = () => {
   const anecdotes = useSelector(state => state)
   // eslint-disable-next-line
