b5b12e54f90050c92f74609faa05e93eb1a0d2af
Eduardo Lange <eduardoandrelange@gmail.com>
2023-10-14 11:47:11 -0300

    Update Notification.js

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,18 +1,15 @@
 import { useSelector } from "react-redux"
 
 const Notification = () => {
-  const notification = useSelector((state) => state.notification); // Replace 'notification' with the actual key in your Redux store
+  const notification = useSelector((state) => state.notification.message); 
+
 
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
-  return (
-    <div style={style}>
-      render here notification...
-    </div>
-  )
+  return notification && <div style={style}>{notification}</div>
 }
 
 export default Notification
