9f3d8f325321021fc9ddae43a34a27829e311d7d
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-10-23 21:55:42 -0300

    6.14 & 6.15

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,37 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit"
 
-const anecdotesAtStart = [
-	"If it hurts, do it more often",
-	"Adding manpower to a late software project makes it later!",
-	"The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-	"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-	"Premature optimization is the root of all evil.",
-	"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-]
 
 const getId = () => Number(100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-	return {
-		content: anecdote,
-		id: getId(),
-		votes: 0,
-	}
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecdoteSlice = createSlice({
-	name: 'anecdotes',
-	initialState,
+	name: "anecdotes",
+	initialState: [],
 	reducers: {
 		createAnecdote(state, action) {
 			const content = action.payload
-			state.push({
-				content,
-				id: getId(),
-				votes: 0
-			})
+			state.push(action.payload)
 		},
 		vote(state, action) {
 			const id = action.payload
@@ -45,15 +23,22 @@ const anecdoteSlice = createSlice({
 				console.log(state) //não funciona
 				console.log(JSON.parse(JSON.stringify(state))) //funciona
 
-
 				return state.map((anecdote) =>
 					anecdote.id === id ? updatedAnecdote : anecdote
 				)
 			}
+		},
+		appendAnecdote(state, action) {
+			state.push(action.payload)
+		},
+		setAnecdotes(state, action) {
+			return action.payload
 		}
-	}
+	},
 })
 
+export const { createAnecdote, vote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
 
 /*
 export const vote = (id) => {
@@ -73,8 +58,6 @@ export const createAnecdote = (content) => {
 		},
 	}
 } */
-
-
 
 /*
 const anecdoteReducer = (state = initialState, action) => {
@@ -100,7 +83,4 @@ const anecdoteReducer = (state = initialState, action) => {
 	}
 }
 
-export default anecdoteReducer */ 
-
-export const { createAnecdote, vote} = anecdoteSlice.actions
-export default anecdoteSlice.reducer
+export default anecdoteReducer */
