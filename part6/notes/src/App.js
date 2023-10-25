d8b2611e56a9c3767f5dda829d697abbc399ac59
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-10-24 21:07:05 -0300

    hell yeah

--- part6/notes/src/App.js
+++ part6/notes/src/App.js
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react"
+import { useEffect } from 'react'
 import Notes from "./components/Notes"
 import NewNote from "./components/NewNote"
-import VisibilityFilter from "./components/VisibilityFilter"
-import noteService from "./services/notes"
-import { setNotes } from "./reducers/noteReducer"
-import { useDispatch } from "react-redux"
+import VisibilityFilter from './components/VisibilityFilter'
+
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
-	const dispatch = useDispatch()
-	useEffect(() => {
-		noteService.getAll().then((notes) => dispatch(setNotes(notes)))
-	}, [])
+  const dispatch = useDispatch()
 
-	return (
-		<div>
-			<NewNote />
-			<VisibilityFilter />
-			<Notes />
-		</div>
-	)
+  useEffect(() => {
+    dispatch(initializeNotes()) 
+  }, [dispatch]) 
+
+  return(
+    <div>
+      <NewNote />
+      <VisibilityFilter />
+      <Notes />
+    </div>
+  )
 }
 
 export default App
