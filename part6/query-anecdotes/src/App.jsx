1f0e51a90ec212a9413cc42ba60f187c475fe756
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-10-27 20:33:43 -0300

    até aqui, ok

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,5 +1,8 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
+
 
 const App = () => {
 
@@ -7,14 +10,19 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
 
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+  
+  const anecdotes = result.data
+  
   return (
     <div>
       <h3>Anecdote app</h3>
