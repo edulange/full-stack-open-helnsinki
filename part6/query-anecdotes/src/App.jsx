1d56baa38faffc9fdf9ec36beacd5545a056d739
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-10-27 20:45:54 -0300

    6.20 ok

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,48 +1,52 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
-
+import AnecdoteForm from "./components/AnecdoteForm"
+import Notification from "./components/Notification"
+import { useQuery } from "@tanstack/react-query"
+import { getAnecdotes } from "./requests"
 
 const App = () => {
+	const handleVote = (anecdote) => {
+		console.log("vote")
+	}
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
+	const result = useQuery({
+		queryKey: ["anecdotes"],
+		queryFn: getAnecdotes,
+    retry: false
+	})
+	console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>Carregando dados</div>
   }
+	if (result.isError) {
+		return <div>anecdote service is not available</div>
+	}
+	const anecdotes = result.data || null
 
-  
-  const result = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: getAnecdotes
-  })
-  console.log(JSON.parse(JSON.stringify(result)))
+	return (
+		<div>
+			<h3>Anecdote app</h3>
 
-  if ( result.isLoading ) {
-    return <div>loading data...</div>
-  }
-  
-  const anecdotes = result.data
-  
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
-  )
+			<Notification />
+			<AnecdoteForm />
+
+			{anecdotes !== null ? (
+				anecdotes.map((anecdote) => (
+					<div key={anecdote.id}>
+						<div>{anecdote.content}</div>
+						<div>
+							has {anecdote.votes}
+							<button onClick={() => handleVote(anecdote)}>
+								vote
+							</button>
+						</div>
+					</div>
+				))
+			) : (
+				<div>Carregando dados...</div>
+			)}
+		</div>
+	)
 }
 
 export default App
