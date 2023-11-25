10d4cce2a366c9db87988aa1a47ba2f94b3986f4
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-11-24 21:38:29 -0300

    Update View.js

--- part7/bloglist-front/src/components/View.js
+++ part7/bloglist-front/src/components/View.js
@@ -26,19 +26,6 @@ const BlogDetails = ({ toggleDetail }) => {
 		marginBottom: 5,
 	}
   
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await userService.getAll();
-        dispatch(setAllUsers(response));
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    };
-
-    fetchUsers();
-  }, [dispatch]);
-
 	useEffect(() => {
 		blogService
 			.getSingleBlog(id)
