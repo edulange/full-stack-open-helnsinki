28b29b4db55cbac20aee580697da228459c3f62b
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-12-02 16:58:45 -0300

    adjusting blogform

--- part7/bloglist-front/src/components/BlogForm.js
+++ part7/bloglist-front/src/components/BlogForm.js
@@ -1,70 +1,58 @@
 import { useState } from 'react'
+/* eslint-disable */
+
+
+const FormField = ({ label, type, value, onChange }) => (
+  <div className="mb-2">
+    <label htmlFor={label.toLowerCase()} className="block text-sm font-medium text-gray-600">
+      {label}
+    </label>
+    <input
+      id={label.toLowerCase()}
+      type={type}
+      name={label.toLowerCase()}
+      value={value}
+      onChange={({ target }) => onChange(target.value)}
+      className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+    />
+  </div>
+);
 
 const BlogForm = ({ createBlog }) => {
-  const [newBlogTitle, setNewBlogTitle] = useState('')
-  const [newBlogAuthor, setNewBlogAuthor] = useState('')
-  const [newBlogUrl, setNewBlogUrl] = useState('')
+  const [newBlogTitle, setNewBlogTitle] = useState('');
+  const [newBlogAuthor, setNewBlogAuthor] = useState('');
+  const [newBlogUrl, setNewBlogUrl] = useState('');
 
   const addBlog = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     createBlog({
       title: newBlogTitle,
       author: newBlogAuthor,
       url: newBlogUrl,
-    })
+    });
 
-    setNewBlogTitle('')
-    setNewBlogAuthor('')
-    setNewBlogUrl('')
-    console.log('veio do componente BlogForm')
+    setNewBlogTitle('');
+    setNewBlogAuthor('');
+    setNewBlogUrl('');
+  };
 
-  }
   return (
-    <div>
-      <h2>Create a new blog</h2>
+    <div className="bg-white p-6 rounded-md shadow-md">
+      <h2 className="text-2xl font-bold mb-2">Create a new blog</h2>
       <form onSubmit={addBlog}>
-        <div>
-          <div>
-						Title:{' '}
-            <input
-              id='title'
-              type="text"
-              name="Title"
-              value={newBlogTitle}
-              onChange={({ target }) =>
-                setNewBlogTitle(target.value)
-              }
-            />
-          </div>
-          <div>
-						Author:{' '}
-            <input
-              id='author'
-              type="text"
-              name="Author"
-              value={newBlogAuthor}
-              onChange={({ target }) =>
-                setNewBlogAuthor(target.value)
-              }
-            />
-          </div>
-          <div>
-						Url:{' '}
-            <input
-              id='url'
-              type="text"
-              name="Url"
-              value={newBlogUrl}
-              onChange={({ target }) =>
-                setNewBlogUrl(target.value)
-              }
-            />
-          </div>
-          <button id='create-btn' type="submit">Create</button>
-        </div>
+        <FormField label="Title" type="text" value={newBlogTitle} onChange={setNewBlogTitle} />
+        <FormField label="Author" type="text" value={newBlogAuthor} onChange={setNewBlogAuthor} />
+        <FormField label="URL" type="text" value={newBlogUrl} onChange={setNewBlogUrl} />
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          Create
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default BlogForm
+export default BlogForm;
